3417285756c032c52f68a2c109fa8176532847a4
Emmanuel <emmanueldavidson524@gmail.com>
2024-09-25 20:43:13 +0100

    first push deployment

---
+++ app/_components/TalentManagement.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TalentManagement() {
+  return (
+    <div>TalentManagement</div>
+  )
+}
---
+++ app/utils/data.js
@@ -0,0 +1,12 @@
+export const data = [
+    {
+        id: 1,
+        clientName: "Emma"
+    },
+    {
+        id: 2,
+        clientName: "Segun"
+    }
+]
+
+//shadcn
